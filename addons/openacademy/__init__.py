44aa6a4aaf7f25b44505322438c4e63ca22fc95c
Felipe Martínez Hermo <felipe@galicia.ugt.org>
2016-03-16 13:27:58 +0100

    20160316: Sustituciones

---
+++ addons/openacademy/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#from addons.openacademy import controllers
+#from addons.openacademy import models
+#from addons.openacademy import partner
+#from addons.openacademy import wizard
+
+import controllers
+import models
+import partner
+import wizard
